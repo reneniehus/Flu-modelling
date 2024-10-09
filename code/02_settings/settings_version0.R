333ae40724ddced0b74d8d88aaf79b10418a14a5
reneniehus <rene.niehus@gmx.de>
2024-10-09 15:27:14 +0200

    edits

--- code/02_settings/settings_version0.R
+++ code/02_settings/settings_version0.R
@@ -23,7 +23,7 @@ settings = function() {
   
   # ---- |-Disease parameters ----
   params$rate_infectious = 0.2777778
-  params$Rnull = 2.0 # https://www.cambridge.org/core/journals/epidemiology-and-infection/article/estimation-of-the-basic-reproductive-number-r0-for-epidemic-highly-pathogenic-avian-influenza-subtype-h5n1-spread/A60F72F5004F3BC5FAC2A3F8BB188A0F
+  params$Rnull = 1.5 # 
   
   # immunity parameters
   params$ve_spread = 0.20 # vaccine effect on onward spread when vaccinated individual is infected
